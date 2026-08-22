821c789f547061d61b2a156fa03b210d46580bb2
satwikakatta08 <satwikakatta08@gmail.com>
2026-08-22 11:30:53 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/First Repeated Character/first-repeated-character.py
@@ -0,0 +1,9 @@
+class Solution:
+    def firstRepChar(self, s):
+        # code here
+        seen=set()
+        for char in s:
+            if char in seen:
+                return char
+            seen.add(char)
+        return '-1'
