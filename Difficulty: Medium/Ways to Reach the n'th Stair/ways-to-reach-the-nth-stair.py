e8c8954fdad151e21a7a8046ae92ec2172143749
satwikakatta08 <satwikakatta08@gmail.com>
2026-08-27 11:07:25 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Ways to Reach the n'th Stair/ways-to-reach-the-nth-stair.py
@@ -0,0 +1,12 @@
+class Solution:
+    def countWays(self, n: int) -> int:
+        # code here
+        if n==1:
+            return 1
+        if n==2:
+            return 2
+        a=1
+        b=2
+        for i in range(3,n+1):
+            a,b=b,a+b
+        return b
